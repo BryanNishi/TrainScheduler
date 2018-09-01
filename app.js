5bcf01b3677d112756bba3e91effd3b415d538de
Bryan Nishi <bryan.nishi@gmail.com>
2018-09-01 17:37:54 -0600

    working code

---
+++ app.js
@@ -0,0 +1,71 @@
+var config = {
+    apiKey: "AIzaSyB-q1eXLugjjoYPfwyal0CsBxL5klrrNSs",
+    authDomain: "train-scheduler-7ec63.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7ec63.firebaseio.com",
+    projectId: "train-scheduler-7ec63",
+    storageBucket: "",
+    messagingSenderId: "585576149296"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = 0;
+var currentTime = moment().format("hh:mm:ss a");
+//load page functions/intervals on load
+clock();
+tickTock();
+//real time clock on header
+function clock(){
+    $("#time").html(moment().format('ddd MMM Do h:mm:ss a'));
+}
+setInterval(clock, 1000);
+
+$("#add-train").on("click", function () {
+
+    // Get inputs
+    trainName = $("#nameInput").val().trim();
+    destination = $("#destInput").val().trim();
+    trainTime = $("#timeInput").val().trim();
+    frequency = $("#freqInput").val().trim();
+
+
+    // Change what is saved in firebase
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+    });
+
+});
+
+
+function tickTock() {
+    
+    $("#table").empty();
+    database.ref().on("child_added", function (childSnapshot) {
+            //establish new time formats from database
+            var freq = parseInt(childSnapshot.val().frequency);
+
+            var formatTime = moment(childSnapshot.val().trainTime, 'HH:mm a').subtract(1, "day");
+
+            var diffTime = moment().diff(formatTime, "minutes");
+
+            var timeLeft = diffTime % freq;
+
+            var tMinutesTillTrain = freq - timeLeft;
+
+            var next = moment().add(tMinutesTillTrain, "minutes");
+
+            var nextTrain = moment(next).format("hh:mm a");
+            $("#table").append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination + "</td><td>" + freq + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>")
+
+
+        }); 
+        
+
+    };
+    setInterval(tickTock, 60000);
